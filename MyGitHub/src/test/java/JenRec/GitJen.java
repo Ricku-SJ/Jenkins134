f358148f4f0e72dba6e6d711281fcff9ded5a495
Ricku-SJ <srinibashjenajnn99@gmail.com>
2023-09-04 17:47:19 +0530

    First Commit

---
+++ MyGitHub/src/test/java/JenRec/GitJen.java
@@ -0,0 +1,14 @@
+package JenRec;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import GitRec.JenXE;
+
+public class GitJen extends JenXE {
+@Test
+	public void createActi()
+	{
+		Reporter.log("Create Acti",true);
+	}
+}
